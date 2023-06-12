2908320b66a28081dd6a471741b4dec79eb0290d
Luca Pagliaro <luca.pagliaro@thewavestudio.it>
2023-06-12 13:02:06 +0200

    feat: added querySelector samples

--- 4- DOM/index.js
+++ 4- DOM/index.js
@@ -1,3 +1,27 @@
 import './style.css';
 
-console.log('Ciao!')
+console.log('Ciao!');
+
+console.log(window); // è l'oggetto globale
+console.log(document); // il DOM
+
+// Come cercare elementi nel mio DOM
+const mioDiv = document.querySelector('.pausa'); // null || primo l'elemento trovato
+console.log(mioDiv);
+
+// controllo se esiste il div
+if(mioDiv){
+    // assegno un "display: block" 
+    mioDiv.style.display = 'block';
+
+    const h1 = mioDiv.querySelector('h1');
+    console.log(h1);
+}
+
+console.log('tag script', document.querySelectorAll('div'));
+
+const allScripts = document.querySelectorAll('script');
+console.log(allScripts);
+// puoi convertire una NodeList in array usando
+const arrayAllScripts = Array.from(allScripts);
+console.log(arrayAllScripts);
